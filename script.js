3cf505b53ccd1e3c75e8369b700c779850cba926
sanjay101103 <sanjaybala003@gmail.com>
2026-07-08 10:45:16 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,46 @@
+AOS.init({
+  duration:1000
+});
+
+/* TYPING */
+
+new Typed(".typing",{
+  strings:[
+    "Cloud Engineer",
+    "DevOps Learner",
+    "AWS Enthusiast"
+  ],
+  typeSpeed:70,
+  backSpeed:50,
+  loop:true
+});
+
+/* PARTICLES */
+
+particlesJS("particles-js",{
+  particles:{
+    number:{
+      value:80
+    },
+    color:{
+      value:"#38bdf8"
+    },
+    shape:{
+      type:"circle"
+    },
+    opacity:{
+      value:0.5
+    },
+    size:{
+      value:3
+    },
+    line_linked:{
+      enable:true,
+      color:"#38bdf8"
+    },
+    move:{
+      enable:true,
+      speed:2
+    }
+  }
+});
